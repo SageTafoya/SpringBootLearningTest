afb6c13b854dd91dd0e2d59ff4c5f99afda0e236
SageTafoya <101155383+SageTafoya@users.noreply.github.com>
2025-11-11 12:19:57 -0800

    can now send post run creation request

--- src/main/java/dev/tafoya/springtest/run/RunController.java
+++ src/main/java/dev/tafoya/springtest/run/RunController.java
@@ -1,14 +1,53 @@
 package dev.tafoya.springtest.run;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.List;
+import java.util.Optional;
+
+//responds to requests, returns responses
 @RestController
-public class RunController {
+@RequestMapping("/api/runs")
+public class RunController
+{
 
-    @GetMapping("/hello")
-    String home(){
-        return "<h1>Hello Runnerz<h1>";
+
+    //Makes Hello message at local host /hello
+    //    @GetMapping("/hello")
+    //    String home(){
+    //        return "<h1>Hello Runnerz<h1>";
+    //    }
+    private final RunRepository runRepository;
+
+
+    public RunController(RunRepository runRepository)
+    {
+        this.runRepository = runRepository;
     }
+
+    @GetMapping("")
+    List<Run> findAll()
+    {
+        return runRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    Run findById(@PathVariable Integer id){
+
+        Optional<Run> run = runRepository.findById(id);
+        if(run.isEmpty()){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "run not found");
+        }
+        return run.get();
+    }
+    //create = post
+    @PostMapping("")
+    void create(@RequestBody Run run){
+        runRepository.create(run);
+    }
+    //update = set
+    //delete = delete
 }
+
---
+++ src/main/java/dev/tafoya/springtest/run/RunRepository.java
@@ -0,0 +1,50 @@
+package dev.tafoya.springtest.run;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class RunRepository {
+    private List<Run> runs = new ArrayList<>();
+
+    List<Run> findAll() {
+        return runs;
+    }
+
+    //may or may not return a non null value
+    Optional<Run> findById(Integer id){
+        return  runs.stream()
+                .filter(run -> run.id() == id)
+                .findFirst();
+    }
+
+    //creating a run
+    void create(Run run){
+        runs.add(run);
+    }
+
+
+    //initalizes for this class
+    @PostConstruct
+    private void init()
+    {
+        runs.add(new Run( 1,
+                "England Run",
+                LocalDateTime.now(),
+                LocalDateTime.now().plus(1, ChronoUnit.HOURS),
+                5,
+                Location.OUTDOOR));
+        runs.add(new Run( 2,
+                "Scotland Run",
+                LocalDateTime.now(),
+                LocalDateTime.now().plus(2, ChronoUnit.HOURS),
+                8,
+                Location.INDOOR));
+    }
+}
